ea89bfec2aafe5892034d18fbf587c719bc9d7c0
TAFH-debug <talim2007@mail.ru>
2024-06-22 21:11:06 +0500

    file upload

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,26 @@ import authRouter from "./auth/auth-router";
 import coursesRouter from "./courses/course-router";
 import adminRouter from "./admin/admin-router";
 import userRouter from "./auth/user-router";
+import fileUpload from "express-fileupload";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
+app.use(express.static("public"));
+
+app.post('/upload', function(req, res) {
+  const req2 = (req as any);
+  req2.files.photo.mv('public/' + req2.files.photo.name);
+  res.status(200).json({ url: req2.files.photo.name });
+});
 
 app.post("/gpt", gpt);
 app.use(adminRouter);
 app.use(userRouter);
 app.use("/", authRouter);
 app.use("/courses", coursesRouter);
+app.post
 
 connectDB();
 
