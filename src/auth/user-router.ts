8fc7f095fc328edf5ec31cf3546856e9e3a70f04
TAFH-debug <talim2007@mail.ru>
2024-06-22 21:49:35 +0500

    mentors x2

--- src/auth/user-router.ts
+++ src/auth/user-router.ts
@@ -6,6 +6,6 @@ const userRouter = Router();
 const userController = new UserController();
 
 userRouter.put('/user/update', authMiddleware, userController.updateSettings);
-userRouter.get("/user/:id", userController.getUserById);
 userRouter.get("/user/mentors", userController.getMentors);
+userRouter.get("/user/:id", userController.getUserById);
 export default userRouter;
