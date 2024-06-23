13f2c7f8c60b7560191c083bb40d126ea01ce5d0
TAFH-debug <talim2007@mail.ru>
2024-06-23 10:55:25 +0500

    eee

--- src/auth/user-router.ts
+++ src/auth/user-router.ts
@@ -5,7 +5,7 @@ import {authMiddleware} from "../middlewares/auth-middleware";
 const userRouter = Router();
 const userController = new UserController();
 
-userRouter.put('/user/update', authMiddleware, userController.updateSettings);
+userRouter.post('/user/update', authMiddleware, userController.updateSettings);
 userRouter.get("/user/mentors", userController.getMentors);
 userRouter.get("/user/:id", userController.getUserById);
 export default userRouter;
